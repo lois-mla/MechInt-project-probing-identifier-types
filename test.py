b7f7842e34d8ae8f2763d91e28261211c45a4b5d
LOIS <lois@lagerweij.com>
2026-01-23 16:29:36 +0100

    FIM trying things

--- test.py
+++ test.py
@@ -19,39 +19,104 @@ model = AutoModelForCausalLM.from_pretrained(
     # torch_dtype="float16"
 )
 
-# # Fill-in-the-middle example
-# prefix = "# function that adds two numbers \ndef "   # Start of function
-# suffix = "(x, y):\n    return x + y\n\n# add 2 and 3 together\nsum = addition(2, 3)"
+def fill_in_middle(prefix: str, suffix: str):
+    # CodeLlama FIM convention: use special <fim-prefix> and <fim-suffix> tokens
+    # The model supports <fim-prefix> and <fim-suffix> for infilling
+    prompt = f"<PRE> {prefix} <SUF>{suffix} <MID>"
 
-prefix = "# set var to 0\n"   # Start of function
-suffix = " = 0\n# add 1 to var\n var +=1 "
+    # prompt = (
+    #     "<fim-prefix>def "
+    #     "<fim-middle>"
+    #     "<fim-suffix>(x, y):\n"
+    #     "    return x + y\n\n"
+    #     "sum = addition(2, 3)"
+    # )
 
-# CodeLlama FIM convention: use special <fim-prefix> and <fim-suffix> tokens
-# The model supports <fim-prefix> and <fim-suffix> for infilling
-prompt = f"<PRE> {prefix} <SUF>{suffix} <MID>"
+    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 
-# prompt = (
-#     "<fim-prefix>def "
-#     "<fim-middle>"
-#     "<fim-suffix>(x, y):\n"
-#     "    return x + y\n\n"
-#     "sum = addition(2, 3)"
-# )
+    # Generate the missing middle
+    outputs = model.generate(
+        **inputs,
+        max_new_tokens=5,
+        do_sample=False,
+        # early_stopping=False,
+        # eos_token_id=None,   # allow generation past EOS prediction
+        # temperature=0.7,
+    )
 
-inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+    print("begin")
+    # print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+    print(tokenizer.decode(outputs[0]))
+    print("end")
 
-# Generate the missing middle
-outputs = model.generate(
-    **inputs,
-    max_new_tokens=10,
-    do_sample=False,
-    # early_stopping=False,
-    # eos_token_id=None,   # allow generation past EOS prediction
-    # temperature=0.7,
-)
 
-# print(tokenizer.decode(outputs[0], skip_special_tokens=True))
-print(tokenizer.decode(outputs[0]))
+# Fill-in-the-middle example
+
+# prefix = "def " 
+# suffix = "(x, y):\n    return x + y\n\nsum = addition(2, 3)"
+# fill_in_middle(prefix, suffix)
+
+# prefix = "# function that adds two numbers \ndef " 
+# suffix = "(x, y):\n    return x + y\n\n# add 2 and 3 together \nsum = addition(2, 3)"
+# fill_in_middle(prefix, suffix)
+
+
+# prefix = "# set var to 0 \n"
+# suffix = " = 0\n# add 1 to var\n var +=1 "
+# fill_in_middle(prefix, suffix)
+
+# prefix = ""
+# suffix = " = 0\nvar +=1 "
+# fill_in_middle(prefix, suffix)
+
+prefix = """def """
+suffix = """(x, y):
+    return x + y
+
+sum = addition(2, 3)
+"""
+
+fill_in_middle(prefix, suffix)
+
+prefix = """# function that adds two numbers
+def """
+suffix = """(x, y):
+    return x + y
+
+# add 2 and 3 together
+sum = addition(2, 3)
+"""
+
+fill_in_middle(prefix, suffix)
+
+prefix = """# set var to 0
+"""
+suffix = """ = 0
+# add 1 to var
+var += 1
+"""
+
+fill_in_middle(prefix, suffix)
+
+prefix = ""
+suffix = """ = 0
+var += 1
+"""
+
+fill_in_middle(prefix, suffix)
+
+
+prefix = """class """ 
+suffix = """:
+    def __init__(self):
+        self.data = []
+
+    def add(self, x):
+        self.data.append(x)
+
+bag = Bag()"""
+
+fill_in_middle(prefix, suffix)
 
 
 # with torch.no_grad():
