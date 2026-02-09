8bf053144b672a0f157e35fae1db0b6996384def
lois-mla <lois@lagerweij.com>
2026-02-09 21:51:49 +0100

    change alpha

--- train_probe.py
+++ train_probe.py
@@ -186,7 +186,7 @@ if __name__ == "__main__":
         prompt=prompt,
         id=0,                    # positive class
         contrastive_id=1,        # negative class
-        alpha=25.0,
+        alpha=50.0,
         layer=layer,                
         resid_type="mlp_out",    # must match extractor
     )
