deb07f90a66e0f1a926cfd6cb6398ab6937ce158
lois-mla <lois@lagerweij.com>
2026-02-27 11:45:22 +0100

    ready to steer

--- train_probe.py
+++ train_probe.py
@@ -462,7 +462,7 @@ v = pot['z']
     print("Test accuracy:", results[best_layer]["test_acc"])
 
     # print("All results:", results)
-    steering_path = "training_data/steering_data_new.txt"
+    steering_path = "training_data_300_2.txt"
     steer_prompts_from_file(steering_path, model, tokenizer, results)
 
 
