7ec97be5fd351cc329733e577b544ccce71d239e
LisaHDo <lisa.douwes@gmail.com>
2026-06-23 23:10:54 +0200

    baseline savedir bug fixed

--- train_probe.py
+++ train_probe.py
@@ -250,13 +250,16 @@ def main():
     # last three lines; contrastive realistic-name dataset
     data_def = "training_data/def_FIM_data_final.txt"
     data_call = "training_data/call_FIM_data_final.txt"
-    probe_save_dir = "probes_stored/probes_final"
+    probe_save_dir = "probes_stored/probes_final" 
     # data_def = "training_data/def_FIM_data_nocont.txt"
     # data_call = "training_data/call_FIM_data_nocont.txt"
     # probe_save_dir = "probes_stored/probes_no_cont"
     # data_def = "training_data/def_FIM_data.txt"
     # data_call = "training_data/call_FIM_data.txt"
     # probe_save_dir = "probes_stored/probes_realistic"
+
+    # for the baseline we need a separate save directory
+    probe_save_dir = "probes_stored/probes_final_baseline"
 
     # specify the name of the chosen dataset for saving the file and plot titles
     dataset_specifier = "cont_baseline"
