903da6cf271ab54f6cf16c53771c7d6a5f4ac579
LisaHDo <lisa.douwes@gmail.com>
2026-06-27 20:52:58 +0200

    bsaeline pt2

--- train_probe.py
+++ train_probe.py
@@ -281,9 +281,9 @@ def main():
     # next three lines; non-contrastive letter-name dataset
     # last three lines; contrastive realistic-name dataset
 
-    data_def = "training_data/def_FIM_data_final.txt"
-    data_call = "training_data/call_FIM_data_final.txt"
-    probe_save_dir = "probes_stored/probes_final" 
+    # data_def = "training_data/def_FIM_data_final.txt"
+    # data_call = "training_data/call_FIM_data_final.txt"
+    # probe_save_dir = "probes_stored/probes_final" 
     # data_def = "training_data/def_FIM_data_nocont.txt"
     # data_call = "training_data/call_FIM_data_nocont.txt"
     # data_def = "training_data/def_FIM_data_final.txt"
@@ -315,8 +315,8 @@ def main():
     # # probe_save_dir = f"probes_stored/{identifier_mode}"
 
     # # specify the name of the chosen dataset for saving the file and plot titles
-    dataset_specifier = "cont_baseline"
-    dataset_specifier_fullname = "contrastive dataset baseline"
+    dataset_specifier = "cont_baseline_resid_post"
+    dataset_specifier_fullname = "contrastive dataset baseline residual post"
     # # # dataset_specifier = "cont_only_correct"
     # # # dataset_specifier_fullname = "contrastive dataset only correct"
     # # # dataset_specifier = "cont_baseline"
@@ -333,6 +333,10 @@ def main():
     device = "cuda"
     n_layers = model.cfg.n_layers
     resid_type = "resid_post" # NOTE: HERE YOU CHOOSE THE LOCATION TO PROBE IN # want to try mlp.hook_post, did mlp_out
+
+    data_def = "training_data/def_FIM_data_final.txt"
+    data_call = "training_data/call_FIM_data_final.txt"
+    probe_save_dir = f"probes_stored/probes_{dataset_specifier}_{resid_type}_baseline"
 
     # for identifier_mode in ['letters', 'common', 'tokenizer']:
     #     data_def = f"datasets/final/{identifier_mode}/mixed_definition.jsonl"
